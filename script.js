45e6b707649fb98d407f8debba4c337d2650e81c
gertahoxha1 <hoxha_gerta14@hotmail.com>
2023-11-30 17:00:14 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+// let titulli = document.getElementById('header')
+// let fotoja = document.querySelector('img')
+let paragrafi1 = document.querySelector('.firstP')
+let paragrafi2 = document.getElementById('.secondP')
+let from = document.querySelector('#from')
+
+// titulli.textContent = 'Urime ditelindjen!'
+// fotoja.src = 'https://hips.hearstapps.com/hmg-prod/images/birthday-wishes-for-daughter-illegal-obnes-1651268615.png'
+// fotoja.alt = 'Foto me java'
+
+// paragrafi1.textContent = 'Te uroj te gjitha te mirat e botes'
+// paragrafi2[1].textContent= 'Shpresoj qe te gjitha deshirat te te plotesohen'
+
+// let titulli= document.getElementById('header')
+
+// titulli.style.fontStyle = 'italic'
+// titulli.style.backgroundColor = 'yellow'
+// titulli.style.color = '#cecece'
+
+// titulli.classList.add("gerta")
+// titulli.classList.remove("gerta")
+
+paragrafi1.classList.add('')
